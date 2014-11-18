8bbc876db4624d00a40dd12f70f8279e381ad618
rfsouto <rfernandezsouto@gmail.com>
2014-11-18 17:37:26 +0100

    First Commit

---
+++ SQLiteDemo/SQLiteDemo/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SQLiteDemo.Resources;
+
+namespace SQLiteDemo
+{
+    /// <summary>
+    /// Proporciona acceso a los recursos de cadena.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ SQLiteDemo/SQLiteDemo/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SQLiteDemo.Resources;
+using SQLitePCL;
+
+namespace SQLiteDemo
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Código de ejemplo para traducir ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var connection = new SQLiteConnection("Storage.db"))
+            {
+                using (var statement = connection.Prepare(@"SELECT * FROM Personas ORDER BY Nombre;"))
+                {
+                    while (statement.Step() == SQLiteResult.ROW)
+                    {
+                        var id = (long) statement[0];
+                        var nombre = (string) statement[1];
+                        var apellido = (string) statement[2];
+                        MessageBox.Show(id.ToString() + "-" + nombre.ToString() + "-" + apellido.ToString());
+                    }
+                }
+            }
+        }
+
+        // Código de ejemplo para compilar una ApplicationBar traducida
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Establecer ApplicationBar de la página en una nueva instancia de ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Crear un nuevo botón y establecer el valor de texto en la cadena traducida de AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Crear un nuevo elemento de menú con la cadena traducida de AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
